ab3f87355a45ddfac8d3fa8179bba952e8070b36
Chinnon Santos <chinnonsantos@gmail.com>
2019-11-24 11:02:43 -0300

    Organizing tests in your folders (drive, assertion, widgets)

--- test/widget/widget_test.dart
+++ test/widget/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:full_testing_flutter/main.dart';
+import 'package:full_testing_flutter/counter.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
