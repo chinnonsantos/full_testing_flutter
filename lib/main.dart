4ff732818d40c4dec25fb7b2d253854642757e80
Chinnon Santos <chinnonsantos@gmail.com>
2019-11-30 12:20:13 -0300

    Updating Provider version (dependency)

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
       // Initialize the model in the builder. That way, Provider
       // can own Counter's lifecycle, making sure to call `dispose`
       // when not needed anymore.
-      builder: (context) => Counter(),
+      create: (context) => Counter(),
       child: MyApp(),
     ),
   );
