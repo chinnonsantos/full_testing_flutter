0db109436c912be7730dec9d2a6b756c66bb0ec6
Chinnon Santos <chinnonsantos@gmail.com>
2019-12-03 20:15:54 -0300

    Including AndroidX support libraries 🍭

---
+++ android/app/src/main/kotlin/br/com/chinnonsantos/full_testing_flutter/full_testing_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.chinnonsantos.full_testing_flutter.full_testing_flutter
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
