06435e6294e0b4a5bfa4f687808cd6c652d84608
Harpreet Sangar <happy_san@protonmail.com>
2020-12-05 21:30:14 +0530

    Migrate to Android embedding v2

--- android/app/src/main/kotlin/br/com/chinnonsantos/full_testing_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/chinnonsantos/full_testing_flutter/MainActivity.kt
@@ -1,12 +1,5 @@
 package br.com.chinnonsantos.full_testing_flutter
 
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
-}
+class MainActivity: FlutterActivity() {}
